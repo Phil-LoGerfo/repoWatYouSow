cd8472789942dcdb4cea454ee60d670dec47b03e
Philip LoGerfo <philip.logerfo@gmail.com>
2021-05-05 13:59:47 -0400

    large sample folder

--- readFilesFromFolder.R
+++ readFilesFromFolder.R
@@ -1,5 +1,8 @@
-temp <- dir(path ="~/TestFolder", full.names = TRUE, recursive = TRUE)
-myfiles = lapply(temp,sep = ",", read.delim)
-dim(myfiles[[1]])
-fileNames <- substring(temp, 40)
-names(myfiles) <- fileNames 
+
+filePaths <- dir(path ="~/Test_set_Lg/Test_Set_Lg", pattern = "*.csv", full.names = TRUE, recursive = TRUE)
+fileNames <- dir(path ="~/Test_set_Lg/Test_Set_Lg", pattern = "*.csv", full.names = FALSE, recursive = TRUE)
+pf = lapply(filePaths,sep = ",", header = TRUE, row.names =1, read.delim)
+dim(pf[[1]])
+names(pf) <- fileNames
+#fileNames <- substring(temp,0)
+#names(myfiles) <- fileNames 
