b0b4b5f503ef6dcca85d7968e56f7f440c09b1c5
Phil-LoGerfo <philip.logerfo@gmail.com>
2021-05-06 16:20:47 -0400

    Handles Empty Datasets

--- readFilesFromFolder.R
+++ readFilesFromFolder.R
@@ -4,5 +4,28 @@ fileNames <- dir(path ="~/Test_set_Lg/Test_Set_Lg", pattern = "*.csv", full.name
 pf = lapply(filePaths,sep = ",", header = TRUE, row.names =1, read.delim)
 dim(pf[[1]])
 names(pf) <- fileNames
+
+pool <- data.frame(name = NULL, sum = NULL)
+
+pnames <- NULL
+psums <- NULL
+for (i in 1:length(names(pf))) {
+  
+  if (dim(pf[[i]])[1] > 0) {
+    tempSum <- sum(pf[[i]][1])
+    psums <- c(psums, tempSum)
+  }
+  
+  else {
+    psums <- c(psums, 0)
+  } 
+    tempName <- names(pf)[[i]]
+    pnames <- c(pnames, tempName)
+}     
+    
+ # pool <- rbind(c(tempName, tempSum))
+
+
+
 #fileNames <- substring(temp,0)
 #names(myfiles) <- fileNames 
